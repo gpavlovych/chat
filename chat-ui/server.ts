a337c4dbfca4aed35100d89ce08896215fcbec82
Heorhii Pavlovych <georgiy.pavlovych@gmail.com>
2024-08-02 13:20:26 +0200

    unused import removed

--- chat-ui/server.ts
+++ chat-ui/server.ts
@@ -4,7 +4,6 @@ import express from 'express';
 import { fileURLToPath } from 'node:url';
 import { dirname, join, resolve } from 'node:path';
 import bootstrap from './src/main.server';
-import express1 from 'express';
 
 // The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
